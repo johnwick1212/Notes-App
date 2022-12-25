fe00c60e27ec52917d01a88351b1f6a800e232f2
kumarpp9718@gmail.com <kumarpp9718@gmail.com>
2022-12-25 19:07:26 +0530

    ...

---
+++ build/precache-manifest.d32183ec2821030dcb2b7dd6d84ab0b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe13dbfab49c6dc82f3aceb9beebf3a",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "bd944ea4a179057518bc",
+    "url": "/Notes-App/static/js/2.dc8daa36.chunk.js"
+  },
+  {
+    "revision": "b97cacf0cb93bfae6d6c",
+    "url": "/Notes-App/static/js/main.889e0f15.chunk.js"
+  },
+  {
+    "revision": "23041eae87cec0a4f203",
+    "url": "/Notes-App/static/js/runtime-main.abc9eb25.js"
+  }
+]);
